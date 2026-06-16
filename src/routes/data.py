9f66a038641435f69882ff742f1b3dbe6fc3a2b3
osamakhaled123 <osamakhaled132000@gmail.com>
2026-06-17 00:31:52 +0300

    create process endpoint

--- src/routes/data.py
+++ src/routes/data.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
 import os
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController, ProcessController
 from models import ResponseSignal
 import aiofiles
 import logging
+from .schemes.data import ProcessingRequest
 
 logger = logging.getLogger("uvicorn.error")
 
@@ -59,3 +60,35 @@ async def upload_file(project_id: str,
             "file_id" :file_id
         }
     )        
+    
+    
+    
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessingRequest):
+    
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+    
+    process_controller = ProcessController(project_id=project_id)
+    
+    file_content = process_controller.get_file_content(file_id=file_id)
+    
+    file_chunks = process_controller.process_file_content(
+        file_id=file_id,
+        file_content=file_content,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+        )
+    
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.PROCESSING_FAILED.value
+            }
+        )
+    
+    return file_chunks 
+    
+    
