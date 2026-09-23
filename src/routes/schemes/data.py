50adf8828cbad46535d076ce784a70cae056cfc0
osamakhaled123 <osamakhaled132000@gmail.com>
2026-09-23 15:44:46 +0300

    adding new request pydantic schemes

--- src/routes/schemes/data.py
+++ src/routes/schemes/data.py
@@ -6,3 +6,10 @@ class ProcessingRequest(BaseModel):
     chunk_size: Optional[int] = 100
     overlap_size: Optional[int] = 20
     do_reset: Optional[int] = 0
+
+class AssetDeletionRequest(BaseModel):
+    asset_name: str
+    file: Optional[bool] = True
+    documentDB: Optional[bool] = False
+    chunks: Optional[bool] = False
+    vectorDB: Optional[bool] = False
