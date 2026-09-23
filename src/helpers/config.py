f2b868140077c64dd628497ce82e5d5a43745be3
osamakhaled123 <osamakhaled132000@gmail.com>
2026-09-23 15:36:23 +0300

    updating configurations

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,7 +4,6 @@ class Settings(BaseSettings):
 
     APP_NAME: str
     APP_VERSION: str
-    OPEN_API_KEY: str
 
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int #MB
@@ -13,6 +12,30 @@ class Settings(BaseSettings):
     
     MONGODB_URL: str
     MONGODB_DATABASE: str
+    
+    INDEXING_PAGE_SIZE: int #BATCH_SIZE
+    
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str
+    OPENAI_BASE_URL: str
+    COHER_API_KEY: str
+
+    GENERATION_MODEL_ID: str
+    EMBEDDING_MODEL_ID: str
+    EMBEDDING_SIZE: int
+
+    INPUT_DEFAULT_MAX_CHARACTERS: int
+    GENERATION_DAFAULT_MAX_TOKENS: int
+    GENERATION_DAFAULT_TEMPERATURE: float
+    
+    VECTOR_DB_BACKEND: str
+    VECTOR_DB_PATH: str
+    VECTOR_DB_DISTANCE_METHOD: str
+
+    PRIMARY_LANG: str
+    DEFAULT_LANG: str
     
     class Config:
         env_file=".env"
